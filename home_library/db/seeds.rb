5ba2097609286dadc9041ad4339d53b6056b92eb
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-09-13 19:50:54 -0500

    rails

--- home_library/db/seeds.rb
+++ home_library/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!([
+  { name: '11.23/63' },
+  { name: 'Eating the Dinosaur' }
+])
